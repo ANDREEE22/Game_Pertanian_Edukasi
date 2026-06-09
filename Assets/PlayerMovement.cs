81e3edb827b039658981ee2dd0dee7537d343be3
Andre <andreandwi345@gmail.com>
2026-06-09 11:47:59 +0700

    menambahkan aset karakter dan UI game pertanian

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,16 +8,47 @@ public class PlayerMovement : MonoBehaviour
     private float moveSpeed = 5f;
     private Rigidbody2D rb;
     private Vector2 moveInput;
+
+    // ----- TAMBAHAN KODE BARU DI SINI -----
+    private Animator anim;
+    private SpriteRenderer spriteRenderer;
+    // --------------------------------------
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        // ----- TAMBAHAN KODE BARU DI SINI -----
+        // Mengambil komponen Animator dan SpriteRenderer dari Player
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        // --------------------------------------
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb .linearVelocity = moveInput * moveSpeed;
+        rb.linearVelocity = moveInput * moveSpeed;
+
+        // ----- TAMBAHAN KODE BARU DI SINI -----
+        // 1. Mengatur Animasi (Idle / Walk)
+        // sqrMagnitude akan bernilai lebih dari 0 jika karakter bergerak (W, A, S, D ditekan)
+        if (anim != null)
+        {
+            anim.SetFloat("Speed", moveInput.sqrMagnitude);
+        }
+
+        // 2. Mengatur Arah Hadap Karakter (Flip)
+        if (moveInput.x < 0)
+        {
+            spriteRenderer.flipX = true;  // Balik ke kiri jika menekan A
+        }
+        else if (moveInput.x > 0)
+        {
+            spriteRenderer.flipX = false; // Hadap kanan normal jika menekan D
+        }
+        // --------------------------------------
     }
 
     public void move(InputAction.CallbackContext context)
